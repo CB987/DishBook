4118b911b6ba0a145d8cfef0e2bbd18f8f3a34a6
Steven Taesung Kim <steventaesungkim@gmail.com>
2018-12-17 15:00:41 -0500

    initial set up for FlagCountry component

---
+++ dishbook/src/FlagCountry.js
@@ -0,0 +1,33 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+
+
+const FlagCountry = () =>{
+    return(
+        <div className = "countryImages">
+            <img alt = "Usa"></img>
+            <br/>
+            <img alt = "Iceland"></img>
+            <br/>
+            <img alt = "Greenland"></img>
+            <br/>
+            <img alt = "Germany"></img>
+            <br/>
+            <img alt = "Italy"></img>
+            <br/>
+            <img alt = "China"></img>
+            <br/>
+            <img alt = "Japan"></img>
+            <br/>
+            <img alt = "Peru"></img>
+            <br/>
+            <img alt = "Canada"></img>
+            <br/>
+            <img alt = "Mexico"></img>
+            <br/>
+            <img alt = "Greece"></img>
+        </div>
+    );
+}
+
+export default FlagCountry;
--- dishbook/src/index.js
+++ dishbook/src/index.js
@@ -1,14 +1,37 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+import {
+    BrowserRouter as Router,
+    Route
+} from 'react-router-dom';
+
+import FlagCountry from './FlagCountry';
+
+
+
 class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            
+        };
+    };
     render() {
         return (
-            <div>
-                <h1>Welcome</h1>
-                <h2>Pick Now</h2>
-            
-            </div>
+            <Router>
+                <div>
+                    <h1>Welcome</h1>
+                    <h2>Pick Now</h2>
+                    <Route path = "/country" render = {(props) =>{
+                        return(<FlagCountry />)
+                    }} />
+
+                   
+                
+                </div>
+
+            </Router>
         );
     }
 }
