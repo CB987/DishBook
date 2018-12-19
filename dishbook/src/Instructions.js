63f2465bf3a870120ac7fc4b8c9dfba73f0a7180
April Copes <antdevelopment1@gmail.com>
2018-12-19 14:51:46 -0500

    plays with width size for ingrediants

--- dishbook/src/Instructions.js
+++ dishbook/src/Instructions.js
@@ -5,7 +5,7 @@ const Instructions = (props) => {
     const measurement = props.amount;
     const instruction = props.instructions;
     const URL = props.sourceURL;
-
+    
     const ingredientList = ingredient.map(ingredient => {
         if (ingredient !== "") {
             return (
